2786b6531a38e3d8de45cc45389f864fca582bef
+dennisngangairungu <+>
2023-10-03 13:15:00 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+const divide = (num1=2000,num2=100) => {
+    if (num1>num2){
+        return num1/num2
+    }
+   
+}
+divide()
+let x=2
+const square = (x) =>{
+    return x*x
+}
+square()
+
+const add = (num1=3,num2=4) => {
+    return num1 + num2
+}
+add()
